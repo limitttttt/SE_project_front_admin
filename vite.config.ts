b2227029f37dc34e05b15c65f77964b07bcfb73f
chhhhhzh <142645264+chhhhhzh@users.noreply.github.com>
2025-04-29 15:11:23 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/SE_project_front/',  // 👈 填你的仓库名（必须正确）
   plugins: [vue()],
 })
