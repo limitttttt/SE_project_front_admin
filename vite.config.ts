f2aa91a03508f120001a45f07160e984a77440ac
Limit <155081658+limitttttt@users.noreply.github.com>
2025-05-19 16:23:56 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/SE_project_front/',  // 👈 填你的仓库名（必须正确）
+  base: '/SE_project_front_admin/',  // 👈 填你的仓库名（必须正确）
   plugins: [vue()],
 })
